fa975f150df37777a9ded94731c74131dea30fed
unknown <edivanjuniorfreitas@gmail.com>
2025-01-29 21:59:25 -0300

    m

--- app/page.js
+++ app/page.js
@@ -1,26 +1,26 @@
-import Image from "next/image";
+import { neon } from "@neondatabase/serverless";
 
-export default function Home() {
+export default async function Home() {
+  const sql = neon("postgres://neondb_owner:npg_TbOZEoW9wiC7@ep-shrill-base-a4jiquhl-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require");
+  const results = await sql('SELECT nome from nome')
   return (
     <div className="overflow-x-auto">
-  <table className="min-w-full border-2 border-black">
-    <thead>
-      <tr>
-        <th className="px-4 py-2 bg-gray-200 text-left text-sm font-medium text-gray-700 uppercase">Aluno</th>
-        <th className="px-4 py-2 bg-gray-200 text-left text-sm font-medium text-gray-700 uppercase">Está no ônibus?</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr className="bg-white border-b">
-        <td className="px-4 py-2 text-sm text-gray-900">João Silva</td>
-        <td className="px-4 py-2 text-sm text-gray-900">Sim</td>
-      </tr>
-      <tr className="bg-gray-50 border-b">
-        <td className="px-4 py-2 text-sm text-gray-900">Maria Oliveira</td>
-        <td className="px-4 py-2 text-sm text-gray-900">Não</td>
-      </tr>
-    </tbody>
-  </table>
-</div>
+      <table className="min-w-full border-2 border-black">
+        <thead>
+          <tr>
+            <th className="px-4 py-2 bg-gray-200 text-left text-sm font-medium text-gray-700 uppercase">Aluno</th>
+            <th className="px-4 py-2 bg-gray-200 text-left text-sm font-medium text-gray-700 uppercase">Está no ônibus?</th>
+          </tr>
+        </thead>
+        <tbody>
+          {results.map((result) => (
+            <tr className="bg-white border-b">
+              <td className="px-4 py-2 text-sm text-gray-900">{result.nome}</td>
+              <td className="px-4 py-2 text-sm text-gray-900"><input type="checkbox"></input></td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
