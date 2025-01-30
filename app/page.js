e38c9e108c478a79b03a75972bc7810d22eb90ee
unknown <edivanjuniorfreitas@gmail.com>
2025-01-30 14:03:17 -0300

    m

--- app/page.js
+++ app/page.js
@@ -1,8 +1,20 @@
+'use client'
+import { useEffect, useState } from "react";
 import { neon } from "@neondatabase/serverless";
 
-export default async function Home() {
-  const sql = neon("postgres://neondb_owner:npg_TbOZEoW9wiC7@ep-shrill-base-a4jiquhl-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require");
-  const results = await sql('SELECT nome from nome')
+export default function Home() {
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const sql = neon("postgres://neondb_owner:npg_TbOZEoW9wiC7@ep-shrill-base-a4jiquhl-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require");
+      const data = await sql("SELECT nome FROM nome");
+      setResults(data);
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="overflow-x-auto">
       <table className="min-w-full border-2 border-black">
@@ -13,10 +25,10 @@ export default async function Home() {
           </tr>
         </thead>
         <tbody>
-          {results.map((result) => (
-            <tr className="bg-white border-b">
+          {results.map((result, index) => (
+            <tr key={index} className="bg-white border-b">
               <td className="px-4 py-2 text-sm text-gray-900">{result.nome}</td>
-              <td className="px-4 py-2 text-sm text-gray-900"><input type="checkbox"></input></td>
+              <td className="px-4 py-2 text-sm text-gray-900"><input type="checkbox" /></td>
             </tr>
           ))}
         </tbody>
